c85e99c988ac0925882e8f47685c9cb74c8d9b7f
Yanet Choque <sol3127g@gmaill.com>
2024-11-12 21:08:58 -0300

    cambios en el index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const cors = require("cors"); // Importa el paquete CORS
 const server = express();
 const PORT = 3001;
 const { conn } = require("./DB_connection");
 const router = require("./routes/index");
+const { conn } = require("./DB_connection");
+const router = require("./routes/index");
 
-// Configurar CORS para permitir solicitudes desde el frontend
 server.use(
   cors({
-    origin: "https://shiny-paletas-d94bbe.netlify.app", // URL de tu frontend en producción
-    credentials: true, // Permitir el uso de cookies y autenticación
+    origin: "https://shiny-paletas-d94bbe.netlify.app",
+    credentials: true, 
   })
 );
 
@@ -21,26 +21,10 @@ conn.sync({ force: false }).then(() => {
     console.info(`listening on port ${PORT}`);
   });
 });
-// const express = require("express");
-// const cors = require("cors"); // Importa el paquete CORS
-// const server = express();
-// const PORT = 3001;
-// const { conn } = require("./DB_connection");
-// const router = require("./routes/index");
-
-// // Configurar CORS para permitir solicitudes desde el frontend
-// server.use(
-//   cors({
-//     origin: "http://localhost:5173", // Aquí defines la URL de tu frontend
-//     credentials: true, // Permitir el uso de cookies y autenticación
-//   })
-// );
 
 // server.use(express.json());
-
-// // Middleware para manejar headers personalizados
 // server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Credentials", "true");
 //   res.header(
 //     "Access-Control-Allow-Headers",
@@ -49,12 +33,10 @@ conn.sync({ force: false }).then(() => {
 //   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 //   next();
 // });
-
-// // Rutas
-// server.use("/rickandmorty", router);
+// server.use("/rickandmorty", router); //este es mejor aca abajo para que todo el midlewore le de permiso al front para acceder a las rutas
 
 // conn.sync({ force: false }).then(() => {
 //   server.listen(PORT, () => {
-//     console.info(`Escuchando en el puerto ${PORT}`);
+//     console.info(`listening on port ${PORT}`);
 //   });
 // });
